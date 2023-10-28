814be8a368722506191ab36dad18854dd0dd45c6
catboomer0419 <z3hyxx211@outlook.com>
2023-10-28 16:44:15 +0800

    add ImGui

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -21,6 +21,10 @@ namespace Hazel {
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 
+		inline Window& GetWindow() { return *m_Window; }
+
+		inline static Application& Get() { return *s_Instance; }
+
 	private:
 		bool OnWindowsClose(WindowCloseEvent& e);
 
@@ -28,6 +32,8 @@ namespace Hazel {
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+	private:
+		static Application* s_Instance;
 	};
 
 	//To be defined in Client
