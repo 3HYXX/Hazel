2d016d6a670eacbf741198325c277c58c999b8ab
catboomer0419 <z3hyxx211@outlook.com>
2023-11-06 15:34:58 +0800

    add GraphicsContext

---
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -0,0 +1,26 @@
+#include "hzpch.h"
+#include "OpenGLContext.h"
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+namespace Hazel {
+
+	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
+		: m_WindowHandle(windowHandle)
+	{
+		HZ_CORE_ASSERT(windowHandle, "WindowHandle is NULL!");
+	}
+
+	void OpenGLContext::Init()
+	{
+		glfwMakeContextCurrent(m_WindowHandle);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "Failed to initailize Glad");
+	}
+
+	void OpenGLContext::SwapBuffers()
+	{
+		glfwSwapBuffers(m_WindowHandle);
+	}
+}
